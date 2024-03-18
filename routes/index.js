7030ff6cdf2ed63baf957898ccff7731130f3357
gelhg08 <angelicamhg31@gmail.com>
2024-03-18 12:46:41 -0500

    actualizar

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,6 @@ router.get("/api/v1/libros", userController.getAllLibros);
 router.get("/api/v1/libro/numeroReferencia/:numeroReferencia", userController.getLibrosByRef);
 router.post("/api/v1/libros", userController.createLibros);
 router.patch("/api/v1/libro/update/:nombre", userController.updateUser);
-router.delete("/api/v1/user/delete/:nombre", userController.deleteUser);
+router.delete("/api/v1/libro/delete/:nombre", userController.deleteUser);
 
 module.exports = router;
